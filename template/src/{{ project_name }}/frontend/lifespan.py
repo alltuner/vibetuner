3000c3be6325a0d0ae57e520ed36414839325cdb
David Poblador i Garcia <davidpoblador@users.noreply.github.com>
2025-05-20 14:31:26 +0200

    include-fastapi (#13)
    * add fastapi fasthx blessed stack
    
    * make start work

---
+++ template/src/{{ project_name }}/frontend/lifespan.py
@@ -0,0 +1,23 @@
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+from .. import __version__
+
+
+class AppContext(BaseModel):
+    version: str = __version__
+    # Add typed state here
+
+    model_config = {"arbitrary_types_allowed": True}
+
+
+ctx: AppContext = AppContext()
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Add the state here cx.state_x = 'a'
+
+    yield
