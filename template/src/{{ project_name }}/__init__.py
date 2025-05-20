46d982e50da5c44487377199c1ee8f5fa8bbb6a4
David Poblador i Garcia <david@poblador.com>
2025-05-20 14:29:01 +0200

    make start work

---
+++ template/src/{{ project_name }}/__init__.py
@@ -0,0 +1,18 @@
+from ._config import (
+    TEMPLATES_DIR,
+    __copyright__,
+    __project_name__,
+    __version__,
+    settings,
+)
+
+__all__ = [
+    "TEMPLATES_DIR",
+    "__copyright__",
+    "__project_name__",
+    "__version__",
+    "settings",
+]
+
+# Your code goes here (add your code between this line and the EOF comment)
+# End of file
---
+++ template/src/{{ project_name }}/_project.py
@@ -0,0 +1,6 @@
+# Note, this file will not be rendered in the materialized scaffolding (the template will win)
+# We keep it here to ensure the imports work
+
+PROJECT_NAME: str = "alltuner-scaffolding"
+
+ENV_PREFIX: str = f"{PROJECT_NAME.upper().replace('-', '_')}_"
