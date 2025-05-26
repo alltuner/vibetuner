18e0d5977370f49d57d99331ac4c05d2cdb0e2c3
David Poblador i Garcia <david@poblador.com>
2025-05-26 11:31:36 +0200

    incorporate castuner improvements

--- template/src/{{ project_name }}/_paths.py
+++ template/src/{{ project_name }}/_paths.py
@@ -3,6 +3,13 @@ from pathlib import Path
 
 ROOT_PROJECT_DIR: Path = Path(__file__).resolve().parent.parent.parent
 
+# Project paths
+root = ROOT_PROJECT_DIR
+assets = ROOT_PROJECT_DIR / "assets"
+locales = ROOT_PROJECT_DIR / "locales"
+
+# Frontend paths
 frontend = ROOT_PROJECT_DIR / "frontend"
 statics = frontend / "statics"
 templates = frontend / "templates"
+markdown = frontend / "markdown"
