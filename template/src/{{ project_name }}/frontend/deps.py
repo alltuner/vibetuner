36a135bff789fa98d5b8456b507aa85b799bfd11
David Poblador i Garcia <davidpoblador@users.noreply.github.com>
2025-05-28 01:31:38 +0200

    minor (#34)
    * ignore certain warnings
    
    * update pyproject
    
    * readapt castuners feedback

---
+++ template/src/{{ project_name }}/frontend/deps.py
@@ -0,0 +1,21 @@
+from typing import Annotated, Optional
+
+from fastapi import Depends, HTTPException, Request
+
+
+async def enforce_lang(request: Request, lang: Optional[str] = None):
+    if lang is None or lang != request.state.language:
+        redirect_url = request.url_for(
+            request.scope["endpoint"].__name__,
+            **{**request.path_params, "lang": request.state.language},
+        ).path
+        raise HTTPException(
+            status_code=307,
+            detail=f"Redirecting to canonical language: {request.state.language}",
+            headers={"Location": redirect_url},
+        )
+
+    return request.state.language
+
+
+LangDep = Annotated[str, Depends(enforce_lang)]
