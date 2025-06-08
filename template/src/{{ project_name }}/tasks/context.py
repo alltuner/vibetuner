6debcb38c08fd30e47d945e8d04094195dbca71a
David Poblador i Garcia <david@poblador.com>
2025-06-08 18:12:10 +0200

    fix task management context

--- template/src/{{ project_name }}/tasks/context.py
+++ template/src/{{ project_name }}/tasks/context.py
@@ -1,11 +1,13 @@
 from contextlib import asynccontextmanager
 from typing import AsyncIterator
 
+from httpx import AsyncClient
 from pydantic import BaseModel
 from streaq import Worker
 
 
 class Context(BaseModel):
+    http_client: AsyncClient
     # Add the context properties for your tasks below
 
     # Until here
@@ -15,10 +17,16 @@ class Context(BaseModel):
 @asynccontextmanager
 async def lifespan(worker: Worker) -> AsyncIterator[Context]:
     # Add below anything that should happen before startup
+
     # Until here
-    yield Context(
-        # Add your context initialization here
-    )
+    async with (
+        AsyncClient() as http_client,
+        # Add any other async context managers you need here
+    ):
+        yield Context(
+            http_client=http_client,
+            # Add any other async context managers you need here
+        )
 
     # Add below anything that should happen before shutdown
     # Until here
