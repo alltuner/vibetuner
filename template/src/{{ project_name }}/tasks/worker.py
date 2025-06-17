3776b967403690160a76f16743e332e7a715bb48
David Poblador i Garcia <davidpoblador@users.noreply.github.com>
2025-06-17 10:39:16 +0200

    email infrastructure (#116)

--- template/src/{{ project_name }}/tasks/worker.py
+++ template/src/{{ project_name }}/tasks/worker.py
@@ -7,9 +7,9 @@ from .context import lifespan
 worker = Worker(
     redis_url=str(project_settings.redis_url),
     queue_name=(
-        project_settings.project_name
+        project_settings.project_slug
         if not settings.debug
-        else f"debug-{project_settings.project_name}"
+        else f"debug-{project_settings.project_slug}"
     ),
     worker_lifespan=lifespan,
 )
