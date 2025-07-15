6da3e6db39f71a9738bc7d07f5e150e92e650fd2
David Poblador i Garcia <david@poblador.com>
2025-07-15 09:57:17 +0200

    harden version

--- template/src/{{ project_slug }}/core/__init__.py
+++ template/src/{{ project_slug }}/core/__init__.py
@@ -2,14 +2,8 @@ from . import paths, templates
 from .config import project_settings, settings
 from .context import Context
 from .logging import logger
+from .version import version
 
-
-try:
-    from ._version import version as __version__
-except ImportError:
-    __version__ = "0.0.0-default"
-
-version = __version__
 
 __all__ = [
     "Context",
