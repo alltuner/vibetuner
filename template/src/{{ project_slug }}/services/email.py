efddb0047b5b5f41db5f88e3e8a5b9c92bf99eb1
David Poblador i Garcia <david@poblador.com>
2025-07-15 10:07:25 +0200

    remove autoimport magic

--- template/src/{{ project_slug }}/services/email.py
+++ template/src/{{ project_slug }}/services/email.py
@@ -2,7 +2,7 @@ from typing import Literal
 
 import boto3
 
-from ..core import project_settings, settings
+from ..core.config import project_settings, settings
 
 
 SES_SERVICE_NAME: Literal["ses"] = "ses"
