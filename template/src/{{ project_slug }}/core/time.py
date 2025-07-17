703174c6db07603aaedfa1d8fb58c59076945fd7
David Poblador i Garcia <davidpoblador@users.noreply.github.com>
2025-07-17 13:57:43 +0200

    minor (#177)
    * abstraction of templates
    
    * user profile

--- template/src/{{ project_slug }}/core/time.py
+++ template/src/{{ project_slug }}/core/time.py
@@ -1,6 +1,7 @@
 from datetime import (
     UTC,
     datetime,
+    timedelta,
 )
 from enum import StrEnum, auto
 
@@ -27,6 +28,30 @@ class Unit(StrEnum):
 
 def now() -> datetime:
     return datetime.now(UTC)
+
+
+def age_in_days(dt: datetime) -> int:
+    # Ensure dt is timezone-aware, if it isn't already
+    if dt.tzinfo is None:
+        dt = dt.replace(tzinfo=UTC)
+
+    return int((now() - dt).total_seconds() / 60 / 60 / 24)
+
+
+def age_in_minutes(dt: datetime) -> int:
+    # Ensure dt is timezone-aware, if it isn't already
+    if dt.tzinfo is None:
+        dt = dt.replace(tzinfo=UTC)
+
+    return int((now() - dt).total_seconds() / 60)
+
+
+def age_in_timedelta(dt: datetime) -> timedelta:
+    # Ensure dt is timezone-aware, if it isn't already
+    if dt.tzinfo is None:
+        dt = dt.replace(tzinfo=UTC)
+
+    return now() - dt
 
 
 # Custom functions below
