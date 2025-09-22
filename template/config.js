1314245b94afb0f75d198c9373cd482e82ba89b6
David Poblador i Garcia <davidpoblador@users.noreply.github.com>
2025-09-22 12:25:47 +0200

    include sse code (#272)

--- template/config.js
+++ template/config.js
@@ -2,5 +2,6 @@
 // Do not change anything between this comment and the next comment
 import "htmx.org";
 import "htmx-ext-preload";
+import "htmx-ext-sse";
 // Do not change anything between this comment and the previous one
 // End of File: do not remove this comment (do not add anything after)
