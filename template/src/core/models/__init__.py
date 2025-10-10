97ee2aadab3fc15a40c242f7180e7758372be0f6
David Poblador i Garcia <david@poblador.com>
2025-10-10 11:03:25 +0200

    relocate core models

--- template/src/core/models/__init__.py
+++ template/src/core/models/__init__.py
@@ -1,6 +1,25 @@
+from beanie import Document, View
+
 from app.models import APP_MODELS
 
-from .core import CORE_MODELS
+from .blob import BlobModel
+from .email_verification import EmailVerificationTokenModel
+from .mixins import TimeStampMixin
+from .oauth import OAuthAccountModel
+from .types import Link
+from .user import UserModel
 
+
+__all__ = [
+    "Link",
+    "TimeStampMixin",
+]
+
+CORE_MODELS: list[type[Document] | type[View]] = [
+    BlobModel,
+    EmailVerificationTokenModel,
+    OAuthAccountModel,
+    UserModel,
+]
 
 MODELS = CORE_MODELS + APP_MODELS
