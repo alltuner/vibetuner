86d6fea1ddeb40fe7f5f4b341afad927b113ccef
David Poblador i Garcia <david@poblador.com>
2025-10-10 10:13:49 +0200

    harden models

--- template/src/core/models/__init__.py
+++ template/src/core/models/__init__.py
@@ -1,12 +1,5 @@
-from beanie import Document, View
-
+from ...app.models import APP_MODELS
 from .core import CORE_MODELS
 
-
-APP_MODELS: list[type[Document] | type[View]] = [
-    # App Specific Models
-]
-
-# Custom Logic such as model rebuilds or migrations can be added here
 
 MODELS = CORE_MODELS + APP_MODELS
