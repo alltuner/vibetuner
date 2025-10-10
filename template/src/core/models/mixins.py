273fb83971414733ab6cf71c9e6109737dba214c
David Poblador i Garcia <david@poblador.com>
2025-10-10 11:23:27 +0200

    fix mixins import paths

--- template/src/core/models/mixins.py
+++ template/src/core/models/mixins.py
@@ -11,7 +11,7 @@ from typing import Self
 from beanie import Insert, Replace, Save, SaveChanges, Update, before_event
 from pydantic import BaseModel, Field
 
-from ...core.time import Unit, now
+from core.core.time import Unit, now
 
 
 class Since(StrEnum):
