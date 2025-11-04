79568fb09371d504b70a45cdd27feb8aa46f6ab3
David Poblador i Garcia <david@poblador.com>
2025-11-04 13:47:34 +0100

    ensure versioning happens in the app context

--- vibetuner-py/src/vibetuner/versioning.py
+++ vibetuner-py/src/vibetuner/versioning.py
@@ -1,7 +1,7 @@
 __version__ = "0.0.0-default"
 
 try:
-    from .._version import version as __version__  # type: ignore
+    from app._version import version as __version__  # type: ignore
 except ImportError:
     pass
 
