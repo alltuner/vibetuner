1b360414a0a82c03dbe3af2b5529ce8ffba06c7a
David Poblador i Garcia <david@poblador.com>
2025-11-12 13:47:25 +0100

    feat: allow for users to extend fastapi's lifespan

--- vibetuner-py/src/vibetuner/frontend/lifespan.py
+++ vibetuner-py/src/vibetuner/frontend/lifespan.py
@@ -2,6 +2,7 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
+from vibetuner.logging import logger
 from vibetuner.mongo import init_models
 
 from .context import ctx
@@ -9,18 +10,22 @@ from .hotreload import hotreload
 
 
 @asynccontextmanager
-async def lifespan(app: FastAPI):
+async def base_lifespan(app: FastAPI):
+    logger.info("Vibetuner frontend starting")
     if ctx.DEBUG:
         await hotreload.startup()
 
     await init_models()
-    # Add below anything that should happen before startup
 
-    # Until here
     yield
 
-    # Add below anything that should happen before shutdown
-
-    # Until here
+    logger.info("Vibetuner frontend stopping")
     if ctx.DEBUG:
         await hotreload.shutdown()
+    logger.info("Vibetuner frontend stopped")
+
+
+try:
+    from app.frontend.lifespan import lifespan
+except ImportError:
+    lifespan = base_lifespan
