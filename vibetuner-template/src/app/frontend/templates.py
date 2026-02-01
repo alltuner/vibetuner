8d3dec8c988dc01e2eb30695912d0dd2eaa0cab5
David Poblador i Garcia <david@poblador.com>
2026-02-01 10:13:31 +0100

    fix: remove the need for custom templates

--- vibetuner-template/src/app/frontend/templates.py
+++
@@ -1,7 +0,0 @@
-# ABOUTME: Custom Jinja2 template filters for the application.
-# ABOUTME: Register filters using the @register_filter() decorator.
-
-from vibetuner.frontend.templates import register_filter  # noqa: F401
-
-# Add your custom filters here
-# See: https://jinja.palletsprojects.com/en/stable/api/#custom-filters
